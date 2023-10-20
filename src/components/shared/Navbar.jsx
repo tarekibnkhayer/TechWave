04ae0963f810e95ee48f5da0d7dfc74d1c1db419
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-10-20 15:22:04 +0600

    navbar active indication implement

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
+import './Navbar.css'
 
 
 const Navbar = () => {
